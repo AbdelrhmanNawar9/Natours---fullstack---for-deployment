4925d44cf9fca547082e4bef643a17fbf203af11
an an <abdonawar97@gmail.com>
2022-08-02 18:33:57 +0200

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -66,6 +66,7 @@ exports.login = catchAsync(async (req, res, next) => {
   }
   // 2) Check if email and password correct
   const user = await User.findOne({ email: email }).select('+password');
+  console.log({ user });
   if (!user || !(await user.correctPassword(password, user.password))) {
     return next(new AppError('Incorrect email or password', 401));
   }
