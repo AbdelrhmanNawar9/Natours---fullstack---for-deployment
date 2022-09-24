a6a3767f85b1a72db8a0726bcc6590aa861c6dbb
an an <abdonawar97@gmail.com>
2022-09-24 16:23:37 +0200

    Initial commit

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ app.options('*', cors());
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public/index.html'));
+// });
 
 // Set security HTTP headers
 // app.use(helmet());
