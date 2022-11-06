4833b2dba6bda2f2435f860094ee55120074393f
an an <abdonawar97@gmail.com>
2022-11-06 08:37:28 +0200

    Inital commit

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ mongoose
   });
 
 // 4) Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
